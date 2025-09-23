97081cb3b50201a7cea1709cecdae4dc934b50e3
souvik mndal <souvkmndal@gmail.com>
2025-09-23 18:00:45 +0530

    read two integers and print their sum

---
+++ 01.Basics/04.Read Two Integers and Print the Sum.cpp
@@ -0,0 +1,13 @@
+// WAP to read two integers and print their sum.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    cout << "The sum is: " << a + b << endl;
+    return 0;
+}
