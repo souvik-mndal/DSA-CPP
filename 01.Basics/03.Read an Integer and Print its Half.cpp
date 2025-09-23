38abb34f6e3d345d30b302064463721952a9ac8c
souvik mndal <souvkmndal@gmail.com>
2025-09-23 17:57:50 +0530

    read an integer and print its half

---
+++ 01.Basics/03.Read an Integer and Print its Half.cpp
@@ -0,0 +1,13 @@
+// WAP to read an integer and print its half.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    cout << "Half of the integer is: " << num / 2 << endl;
+    return 0;
+}
