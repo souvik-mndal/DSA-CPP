268e6203651e1e5b3a258ea0595bf75df328adb8
souvik mndal <souvkmndal@gmail.com>
2025-09-23 17:55:31 +0530

    read and print an integer

---
+++ 01.Basics/02.Read and Print an Integer.cpp
@@ -0,0 +1,13 @@
+// WAP to read an integer and print it.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    cout << "You entered: " << num << endl;
+    return 0;
+}
