051d60be2136d7827c944e69a5beaf8b4318c87e
souvik mndal <souvkmndal@gmail.com>
2025-09-23 19:51:17 +0530

    read a number n and print counting from 1 to n

---
+++ 01.Basics/13.print a number upto n.cpp
@@ -0,0 +1,17 @@
+// WAP to read a number n and print counting from 1 to n.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
