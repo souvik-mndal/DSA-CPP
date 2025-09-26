bb4647ab958b295807f889cb3c63852f12d200c3
souvik mndal <souvkmndal@gmail.com>
2025-09-27 03:21:39 +0530

    read a number n and print a hollow square pattern of '*' of size n x n

---
+++ 01.Basics/18.solid hollow square pattern.cpp
@@ -0,0 +1,30 @@
+// WAP to read a number n and print a hollow square pattern of '*' of size n x n.
+
+/*
+* * * *
+*     *
+*     *
+* * * *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of hollow square: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
